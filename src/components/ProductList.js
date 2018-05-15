2829eb63ad0eff8c2f8e3342f453a838ee1b33e6
mtshv <i.mityushev@gmail.com>
2018-05-15 21:51:01 +0300

    project cleanUp. 1st component was added

---
+++ src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+          Hello! I am ProductList component.
+      </div>
+    );
+  }
+}
+
+export default ProductList;
