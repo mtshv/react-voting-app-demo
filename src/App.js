34060545bcc205bacfde61fde032fae0f3407b39
mtshv <i.mityushev@gmail.com>
2018-05-15 22:25:55 +0300

    Identation fix

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import ProductList from "./components/ProductList";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="main ui text container">
-        <h1 className="ui dividing centered header">Popular Products</h1>
-        <ProductList/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="main ui text container">
+                <h1 className="ui dividing centered header">Popular Products</h1>
+                <ProductList />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import Product from "./Product";
 
 class ProductList extends Component {
-  render() {
-    return (
-      <div className='ui unstackable items'>
-        <Product/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='ui unstackable items'>
+                <Product />
+            </div>
+        );
+    }
 }
 
 export default ProductList;
